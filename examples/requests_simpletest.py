86eb53080af9f583527a6a9015cc30f24220f9be
brentru <robots199@me.com>
2019-07-25 16:40:06 -0400

    add a better simpletest

--- examples/requests_simpletest.py
+++ examples/requests_simpletest.py
@@ -28,10 +28,12 @@ while not esp.is_connected:
         continue
 print("Connected to", str(esp.ssid, 'utf-8'), "\tRSSI:", esp.rssi)
 
+# Initialize a requests object with a socket and esp32spi interface
 requests.set_socket(socket, esp)
 
-# Initialize a requests object with a socket and esp32spi interface
 TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
+JSON_GET_URL = "http://httpbin.org/get"
+JSON_POST_URL = "http://httpbin.org/post"
 
 print("Fetching text from %s"%TEXT_URL)
 response = requests.get(TEXT_URL)
@@ -39,15 +41,34 @@ print('-'*40)
 
 print("Text Response: ", response.text)
 print('-'*40)
+response.close()
 
-print("Raw Response, as bytes: {0}".format(response.content))
+print("Fetching JSON data from %s"%JSON_GET_URL)
+response = requests.get(JSON_GET_URL)
 print('-'*40)
 
-print("Response's HTTP Status Code: %d"%response.status_code)
+print("JSON Response: ", response.json())
+print('-'*40)
+response.close()
+
+data = '31F'
+print("POSTing data to {0}: {1}".format(JSON_POST_URL, data))
+response = requests.post(JSON_POST_URL, data=data)
 print('-'*40)
 
-print("Response's HTTP Headers: %s"%response.headers)
+json_resp = response.json()
+# Parse out the 'data' key from json_resp dict.
+print("Data received from server:", json_resp['data'])
+print('-'*40)
+response.close()
+
+json_data = {"Date" : "July 25, 2019"}
+print("POSTing data to {0}: {1}".format(JSON_POST_URL, json_data))
+response = requests.post(JSON_POST_URL, json=json_data)
 print('-'*40)
 
-# Close, delete and collect the response data
+json_resp = response.json()
+# Parse out the 'json' key from json_resp dict.
+print("JSON Data received from server:", json_resp['json'])
+print('-'*40)
 response.close()
