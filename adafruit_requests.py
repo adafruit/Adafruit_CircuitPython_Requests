2bbf61a5bdd3414787b1279ab7c2088d9ad48dcb
Scott Shawcroft <scott@tannewt.org>
2020-02-04 18:03:01 -0800

    Switch to HTTP1.1 so that we can reuse sockets.

--- adafruit_requests.py
+++ adafruit_requests.py
@@ -54,22 +54,40 @@ import gc
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_Requests.git"
 
-_the_interface = None  # pylint: disable=invalid-name
-_the_sock = None  # pylint: disable=invalid-name
+# This module mirror CPython's socket class. It is settable because the network devices are external
+# to CircuitPython.
+_socket = None  # pylint: disable=invalid-name
 
 
-def set_socket(sock, iface=None):
-    """Helper to set the global socket and optionally set the global network interface.
-    :param sock: socket object.
-    :param iface: internet interface object
+def set_socket(socket):
+    """Helper to set the global socket.
+    :param sock: socket module
 
     """
-    global _the_sock  # pylint: disable=invalid-name, global-statement
-    _the_sock = sock
-    if iface:
-        global _the_interface # pylint: disable=invalid-name, global-statement
-        _the_interface = iface
-        _the_sock.set_interface(iface)
+    global _socket  # pylint: disable=invalid-name, global-statement
+    _socket = socket
+
+# Hang onto open sockets so that we can reuse them.
+_socket_pool = {}
+def _get_socket(host, port, proto, *, timeout=1):
+    key = (host, port, proto)
+    if key in _socket_pool:
+        socket = _socket_pool[key]
+        if not socket.connected():
+            del _socket_pool[key]
+        else:
+            return socket
+    addr_info = _socket.getaddrinfo(host, port, 0, _socket.SOCK_STREAM)[0]
+    sock = _socket.socket(addr_info[0], addr_info[1], addr_info[2])
+    sock.settimeout(timeout)  # socket read timeout
+
+    if proto == "https:":
+        # for SSL we need to know the host name
+        sock.connect((host, port), _socket.TLS_MODE)
+    else:
+        sock.connect(addr_info[-1], _socket.TCP_MODE)
+    _socket_pool[key] = sock
+    return sock
 
 class Response:
     """The response from a request, contains all the headers/content"""
@@ -80,10 +98,20 @@ class Response:
         self.socket = sock
         self.encoding = "utf-8"
         self._cached = None
-        self.status_code = None
-        self.reason = None
-        self._read_so_far = 0
-        self.headers = {}
+        self._headers = {}
+
+        if not self.socket.connected():
+            raise RuntimeError("We were too slow")
+
+        line = self.socket.readline()
+        if line is None:
+            raise RuntimeError("Unable to read HTTP response.")
+        line = line.split(b" ", 2)
+        self.status_code = int(line[1])
+        self.reason = ""
+        if len(line) > 2:
+            self.reason = line[2].rstrip()
+        self._parse_headers()
 
     def __enter__(self):
         return self
@@ -94,26 +122,83 @@ class Response:
     def close(self):
         """Close, delete and collect the response data"""
         if self.socket:
-            self.socket.close()
-            del self.socket
+            # Make sure we've read all of our response.
+            content_length = None
+            if "content-length" in self.headers:
+                content_length = int(self.headers["content-length"])
+
+            # print("Content length:", content_length)
+            if self._cached is None:
+                if content_length:
+                    self.socket.recv(content_length)
+                else:
+                    while True:
+                        chunk_header = self.socket.readline()
+                        if b";" in chunk_header:
+                            chunk_header = chunk_header.split(b";")[0]
+                        chunk_size = int(chunk_header, 16)
+                        if chunk_size == 0:
+                            break
+                        self.socket.read(chunk_size + 2)
+                    self._parse_headers()
+                self.socket = None
         del self._cached
         gc.collect()
+
+    def _parse_headers(self):
+        """
+        Parses the header portion of an HTTP request/response from the socket.
+        Expects first line of HTTP request/response to have been read already.
+        """
+        while True:
+            line = self.socket.readline()
+            if not line or line == b"\r\n":
+                break
+
+            #print("**line: ", line)
+            splits = line.split(b': ', 1)
+            title = splits[0]
+            content = ''
+            if len(splits) > 1:
+                content = splits[1]
+            if title and content:
+                title = str(title.lower(), 'utf-8')
+                content = str(content, 'utf-8')
+                self._headers[title] = content
+
+    @property
+    def headers(self):
+        return self._headers
 
     @property
     def content(self):
         """The HTTP content direct from the socket, as bytes"""
         # print(self.headers)
-        try:
+        content_length = None
+        if "content-length" in self.headers:
             content_length = int(self.headers["content-length"])
-        except KeyError:
-            content_length = 0
+
         # print("Content length:", content_length)
         if self._cached is None:
-            try:
+            if content_length:
                 self._cached = self.socket.recv(content_length)
-            finally:
-                self.socket.close()
-                self.socket = None
+            else:
+                chunks = []
+                while True:
+                    chunk_header = self.socket.readline()
+                    if chunk_header is None:
+                        raise RuntimeError("Reading content timed out.")
+                    if b";" in chunk_header:
+                        chunk_header = chunk_header.split(b";")[0]
+                    chunk_size = int(chunk_header, 16)
+                    if chunk_size == 0:
+                        break
+                    chunks.append(self.socket.read(chunk_size))
+                    self.socket.read(2) # Read the trailing CR LF
+                self._parse_headers()
+                self._cached = b"".join(chunks)
+            self.socket = None
+
         # print("Buffer length:", len(self._cached))
         return self._cached
 
@@ -129,6 +214,7 @@ class Response:
             import json as json_module
         except ImportError:
             import ujson as json_module
+
         return json_module.loads(self.content)
 
     def iter_content(self, chunk_size=1, decode_unicode=False):
@@ -137,16 +223,47 @@ class Response:
         if decode_unicode:
             raise NotImplementedError("Unicode not supported")
 
-        while True:
-            chunk = self.socket.recv(chunk_size)
-            if chunk:
+        content_length = None
+        if "content-length" in self.headers:
+            content_length = int(self.headers["content-length"])
+
+        total_read = 0
+        if content_length:
+            while total_read < content_length:
+                chunk = self.socket.recv(chunk_size)
+                total_read += chunk_size
                 yield chunk
-            else:
-                return
+        else:
+            pending_bytes = 0
+            chunks = []
+            while True:
+                chunk_header = self.socket.readline()
+                if b";" in chunk_header:
+                    chunk_header = chunk_header.split(b";")[0]
+                http_chunk_size = int(chunk_header, 16)
+                if http_chunk_size == 0:
+                    break
+                remaining_in_http_chunk = http_chunk_size
+                while remaining_in_http_chunk:
+                    read_now = chunk_size - pending_bytes
+                    if read_now > remaining_in_http_chunk:
+                        read_now = remaining_in_http_chunk
+                    chunks.append(self.socket.read(read_now))
+                    pending_bytes += read_now
+                    if pending_bytes == chunk_size:
+                        yield b"".join(chunks)
+                        pending_bytes = 0
+                        chunks = []
+
+                self.socket.read(2) # Read the trailing CR LF
+            self._parse_headers()
+            if chunks:
+                yield b"".join(chunks)
+        self.socket = None
 
 
 # pylint: disable=too-many-branches, too-many-statements, unused-argument, too-many-arguments, too-many-locals
-def request(method, url, data=None, json=None, headers=None, stream=False, timeout=1):
+def request(method, url, data=None, json=None, headers=None, stream=False, timeout=60):
     """Perform an HTTP request to the given url which we will parse to determine
     whether to use SSL ('https://') or not. We can also send some provided 'data'
     or a json dictionary which we will stringify. 'headers' is optional HTTP headers
@@ -154,7 +271,7 @@ def request(method, url, data=None, json=None, headers=None, stream=False, timeo
     read only when requested
     """
     global _the_interface  # pylint: disable=global-statement, invalid-name
-    global _the_sock  # pylint: disable=global-statement, invalid-name
+    global _socket  # pylint: disable=global-statement, invalid-name
 
     if not headers:
         headers = {}
@@ -177,92 +294,37 @@ def request(method, url, data=None, json=None, headers=None, stream=False, timeo
         host, port = host.split(":", 1)
         port = int(port)
 
-    addr_info = _the_sock.getaddrinfo(host, port, 0, _the_sock.SOCK_STREAM)[0]
-    sock = _the_sock.socket(addr_info[0], addr_info[1], addr_info[2])
-    resp = Response(sock)  # our response
+    socket = _get_socket(host, port, proto, timeout=timeout)
+    socket.send(b"%s /%s HTTP/1.1\r\n" % (bytes(method, "utf-8"), bytes(path, "utf-8")))
+    if "Host" not in headers:
+        socket.send(b"Host: %s\r\n" % bytes(host, "utf-8"))
+    if "User-Agent" not in headers:
+        socket.send(b"User-Agent: Adafruit CircuitPython\r\n")
+    # Iterate over keys to avoid tuple alloc
+    for k in headers:
+        socket.send(k.encode())
+        socket.send(b": ")
+        socket.send(headers[k].encode())
+        socket.send(b"\r\n")
+    if json is not None:
+        assert data is None
+        try:
+            import json as json_module
+        except ImportError:
+            import ujson as json_module
+        data = json_module.dumps(json)
+        socket.send(b"Content-Type: application/json\r\n")
+    if data:
+        socket.send(b"Content-Length: %d\r\n" % len(data))
+    socket.send(b"\r\n")
+    if data:
+        socket.send(bytes(data, "utf-8"))
 
-    sock.settimeout(timeout)  # socket read timeout
+    resp = Response(socket)  # our response
+    if "location" in resp.headers and not 200 <= resp.status_code <= 299:
+        raise NotImplementedError("Redirects not yet supported")
 
-    try:
-        if proto == "https:":
-            conntype = _the_interface.TLS_MODE
-            sock.connect(
-                (host, port), conntype
-            )  # for SSL we need to know the host name
-        else:
-            conntype = _the_interface.TCP_MODE
-            sock.connect(addr_info[-1], conntype)
-        sock.send(b"%s /%s HTTP/1.0\r\n" % (bytes(method, "utf-8"), bytes(path, "utf-8")))
-        if "Host" not in headers:
-            sock.send(b"Host: %s\r\n" % bytes(host, "utf-8"))
-        if "User-Agent" not in headers:
-            sock.send(b"User-Agent: Adafruit CircuitPython\r\n")
-        # Iterate over keys to avoid tuple alloc
-        for k in headers:
-            sock.send(k.encode())
-            sock.send(b": ")
-            sock.send(headers[k].encode())
-            sock.send(b"\r\n")
-        if json is not None:
-            assert data is None
-            try:
-                import json as json_module
-            except ImportError:
-                import ujson as json_module
-            data = json_module.dumps(json)
-            sock.send(b"Content-Type: application/json\r\n")
-        if data:
-            sock.send(b"Content-Length: %d\r\n" % len(data))
-        sock.send(b"\r\n")
-        if data:
-            sock.send(bytes(data, "utf-8"))
-
-        line = sock.readline()
-        # print(line)
-        line = line.split(None, 2)
-        status = int(line[1])
-        reason = ""
-        if len(line) > 2:
-            reason = line[2].rstrip()
-        resp.headers = parse_headers(sock)
-        if resp.headers.get("transfer-encoding"):
-            if "chunked" in resp.headers.get("transfer-encoding"):
-                raise ValueError("Unsupported " + resp.headers.get("transfer-encoding"))
-        elif resp.headers.get("location") and not 200 <= status <= 299:
-            raise NotImplementedError("Redirects not yet supported")
-
-    except:
-        sock.close()
-        raise
-
-    resp.status_code = status
-    resp.reason = reason
     return resp
-
-def parse_headers(sock):
-    """
-    Parses the header portion of an HTTP request/response from the socket.
-    Expects first line of HTTP request/response to have been read already
-    return: header dictionary
-    rtype: Dict
-    """
-    headers = {}
-    while True:
-        line = sock.readline()
-        if not line or line == b"\r\n":
-            break
-
-        #print("**line: ", line)
-        splits = line.split(b': ', 1)
-        title = splits[0]
-        content = ''
-        if len(splits) > 1:
-            content = splits[1]
-        if title and content:
-            title = str(title.lower(), 'utf-8')
-            content = str(content, 'utf-8')
-            headers[title] = content
-    return headers
 
 def head(url, **kw):
     """Send HTTP HEAD request"""
