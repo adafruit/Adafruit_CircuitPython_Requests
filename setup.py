5b3a2e7c08044fa24a1fc900457262243b9c87d1
AHerrmann <alex.herrmann@globalpay.com>
2022-08-08 10:34:20 -0600

    Take dan halbert's code and bring it in.

--- setup.py
+++ setup.py
@@ -53,5 +53,5 @@ setup(
     # simple. Or you can use find_packages().
     # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
     #       CHANGE `py_modules=['...']` TO `packages=['...']`
-    py_modules=["adafruit_requests"],
+    py_modules=["adafruit_requests", "adafruit_async_requests"],
 )
