211c2d882ffaf0af94dafe6c0e6a60cae1f2b5b7
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2024-03-25 05:17:06 -0400

    update requests_wifi_advanced to 9.0 with Connection Manager

--- examples/wifi/requests_wifi_advanced.py
+++ examples/wifi/requests_wifi_advanced.py
@@ -1,10 +1,12 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
+# Updated for Circuit Python 9.0
+""" WiFi Advanced Example """
+# pylint: disable=import-error
 
 import os
-import ssl
 
-import socketpool
+import adafruit_connection_manager
 import wifi
 
 import adafruit_requests
@@ -13,39 +15,41 @@ import adafruit_requests
 ssid = os.getenv("CIRCUITPY_WIFI_SSID")
 password = os.getenv("CIRCUITPY_WIFI_PASSWORD")
 
-# Initialize WiFi Pool (There can be only 1 pool & top of script)
-radio = wifi.radio
-pool = socketpool.SocketPool(radio)
+TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
+JSON_GET_URL = "https://httpbin.org/get"
+JSON_POST_URL = "https://httpbin.org/post"
 
-print("Connecting to AP...")
-while not wifi.radio.ipv4_address:
-    try:
-        wifi.radio.connect(ssid, password)
-    except ConnectionError as e:
-        print("could not connect to AP, retrying: ", e)
-print("Connected to", str(radio.ap_info.ssid, "utf-8"), "\tRSSI:", radio.ap_info.rssi)
-
-# Initialize a requests session
-ssl_context = ssl.create_default_context()
+# Initalize Wifi, Socket Pool, Request Session
+pool = adafruit_connection_manager.get_radio_socketpool(wifi.radio)
+ssl_context = adafruit_connection_manager.get_radio_ssl_context(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl_context)
+rssi = wifi.radio.ap_info.rssi
+
+print(f"\nConnecting to {ssid}...")
+print(f"Signal Strength: {rssi}")
+try:
+    # Connect to the Wi-Fi network
+    wifi.radio.connect(ssid, password)
+except OSError as e:
+    print(f"❌ OSError: {e}")
+print("✅ Wifi!")
 
 JSON_GET_URL = "https://httpbin.org/get"
 
 # Define a custom header as a dict.
 headers = {"user-agent": "blinka/1.0.0"}
 
-print("Fetching JSON data from %s..." % JSON_GET_URL)
+print(f" | Fetching JSON: {JSON_GET_URL}")
 response = requests.get(JSON_GET_URL, headers=headers)
-print("-" * 60)
 
 json_data = response.json()
 headers = json_data["headers"]
-print("Response's Custom User-Agent Header: {0}".format(headers["User-Agent"]))
-print("-" * 60)
+print(f" | ✅ Response Custom User-Agent Header: {headers['User-Agent']}")
 
 # Read Response's HTTP status code
-print("Response HTTP Status Code: ", response.status_code)
-print("-" * 60)
+print(f" | ✅ Response HTTP Status Code: {response.status_code}")
 
 # Close, delete and collect the response data
 response.close()
+print(f" | ✂️ Disconnected from {JSON_GET_URL}")
+print("Finished!")
